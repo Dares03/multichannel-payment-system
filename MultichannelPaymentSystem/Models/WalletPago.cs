f936371c6c45bfc8361cc9bfbdae34498ecd310f
Dares03 <josshuadavid5g@gmail.com>
2026-05-11 23:24:57 -0500

    feat(Models): Agregando la clase WalletPago con la herencia de MetodoPago

---
+++ MultichannelPaymentSystem/Models/WalletPago.cs
@@ -0,0 +1,17 @@
+namespace MultichannelPaymentSystem.Models;
+
+class WalletPago : MetodoPago
+{
+    public string WalletAddress {get; init;}
+
+    public WalletPago(string id, string titular, decimal monto, string walletAddress) 
+    : base(id, titular, monto)
+    {
+        WalletAddress = walletAddress;
+    }
+
+    public override void ProcesarPago()
+    {
+        Console.WriteLine($"Procesando pago de {Titular} por {Monto:C}...");
+    }
+}
